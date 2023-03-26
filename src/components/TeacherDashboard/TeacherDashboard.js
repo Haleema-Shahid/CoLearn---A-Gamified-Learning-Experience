d236bb05dd072027f93da177fc8dfe07b20b278e
aleen357 <59262976+aleen357@users.noreply.github.com>
2023-03-26 16:21:50 +0500

    teacherdashboard create class form updated

--- src/components/TeacherDashboard/TeacherDashboard.js
+++ src/components/TeacherDashboard/TeacherDashboard.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import './TeacherDashboard.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import TeacherDashboardCard from './TeacherDashboardCard';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import { useNavigate } from 'react-router-dom';
 
- 
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+
+
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 function TeacherDashboard() {
-  const { userID} = useParams();
+  const { userID } = useParams();
   const [showCreateClassModal, setShowCreateClassModal] = useState(false);
   const [className, setClassName] = useState('');
   const [section, setSection] = useState('');
-  const [classes, setClasses] = useState([{ name: "Advanced Programming", section: "B",id:"A2561" }, { name: "Database", section: "B", id:"A2562"}, { name: "Programming Fundamentals", section: "B", id:"A2563" }]);
+  const [classes, setClasses] = useState([{ name: "Advanced Programming", section: "B", id: "A2561" }, { name: "Database", section: "B", id: "A2562" }, { name: "Programming Fundamentals", section: "B", id: "A2563" }]);
 
   const [open, setOpen] = useState(false);
   const [createNewClassButton, setCreateNewClassButton] = useState(false);
 
- 
+
   const handleCreateClassClick = () => {
     setShowCreateClassModal(true);
     setCreateNewClassButton(true)
@@ -55,14 +59,13 @@ function TeacherDashboard() {
       return;
     }
     //checking if this class name and section already exists
-    if(classes.filter(cls => cls.name === className && cls.section === section).length>0)
-    {
+    if (classes.filter(cls => cls.name === className && cls.section === section).length > 0) {
       console.log("entered");
       setOpen(true);
       return
-     
+
     }
-    
+
 
     // Create new class from backend and fetch in front end again
     setClasses([...classes, { name: className, section }]);
@@ -70,27 +73,27 @@ function TeacherDashboard() {
     setClassName('');
     setSection('');
 
-    
+
     // const requestOptions = {
     //   method: 'POST',
     //   headers: { 'Content-Type': 'application/json' },
     //   body: JSON.stringify({ userID:userID, className:className, section: section })
     // };
-  
+
     // const response = await fetch('http://localhost:4000/api/login', requestOptions);
     // const data = await response.json();
-    
+
     // if (response.ok) {
     //   // Do something with the user data
     //   //console.log(data._id);
-     
+
 
     // } else {
     //   // Handle the error
     //   console.log('Error:', response.status);
     // }
 
-    
+
 
   };
 
@@ -107,53 +110,81 @@ function TeacherDashboard() {
 
   return (
     <div className='TeacherDashboard'>
-   
+
       <button onClick={handleCreateClassClick} className="create-class-button" hidden={createNewClassButton}>Create New Class</button>
       {open && (
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-              This Class Exists Already!
-            </Alert>
-          </Snackbar>
-        )}
-      
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            This Class Exists Already!
+          </Alert>
+        </Snackbar>
+      )}
+
       {showCreateClassModal && (
 
-        <div className="create-class-modal">
-          <form onSubmit={handleCreateClassSubmit} className="create-class-form">
-            <label htmlFor="class-name-input">Class Name:</label>
-            <input
-              id="class-name-input"
-              type="text"
-              value={className}
-              onChange={handleClassNameChange}
-            />
-            <br />
-            <label htmlFor="section-input">Section:</label>
-            <input
-              id="section-input"
-              type="text"
-              value={section}
-              onChange={handleSectionChange}
-            />
-            <br />
-            <button type="submit" className='create-button'>Create Class</button>
-            <button onClick={handleCancelClick} className="cancel-button">
-              Cancel
-            </button>
-          </form>
+        <div >
+          <Box  component="form" onSubmit={handleCreateClassSubmit}
+            sx={{
+              margin: "auto",
+              display: "flex",
+              flexDirection: "column",
+
+              paddingLeft: "50px",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Stack spacing={2}>
+              <TextField
+                required
+                id="className"
+                label="Class Name"
+                value={className}
+                onChange={handleClassNameChange}
+                fullWidth
+              />
+              <TextField
+                required
+                id="sectionInput"
+                label="Section"
+                value={section}
+                onChange={handleSectionChange}
+                fullWidth
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  backgroundColor: '#1e3c72',
+                  color: 'white',
+                  borderRadius: '10px',
+                  padding: '10px 30px',
+                  fontSize: '1rem',
+                  '&:hover': {
+                    backgroundColor: '#0c2461',
+                  },
+                }}
+              >
+                Create Class
+              </Button>
+              <Button onClick={handleCancelClick} variant="outlined">
+                Cancel
+              </Button>
+            </Stack>
+          </Box>
+
         </div>
-        
-        
+
+
       )}
       {!showCreateClassModal && (
         <div className="classes-grid">
           {classes.map((classObj) => (
-            // <Link to={`/user/${userID}/class/${classObj.id}`} key={classObj.id}>
-            <div className="container_card" key={classObj.name + classObj.section} >
-             <TeacherDashboardCard name={classObj.name} section={classObj.section} id={classObj.id} userID={userID} onDelete={handleDeleteClass }/>
-            </div>
-            // </Link>
+            <Link to={`/user/${userID}/class/${classObj.id}`} key={classObj.id}>
+              <div className="container_card" key={classObj.name + classObj.section} >
+                <TeacherDashboardCard name={classObj.name} section={classObj.section} id={classObj.id} userID={userID} onDelete={handleDeleteClass} />
+              </div>
+            </Link>
           ))}
         </div>
       )}
