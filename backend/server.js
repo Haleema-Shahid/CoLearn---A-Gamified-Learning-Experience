7fb7d7412b4746ef9597983964f57054ef014ca2
Haleema-Shahid <h.specs01@gmail.com>
2023-06-04 22:26:50 +0500

    server connects to mongodb using mongoose instead of vscode extension

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
-const express = require('express')
-const app = express()
-//const mongoose = require('mongoose')
-const routesUrls = require('./routes/route')
-const dotenv = require('dotenv')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const routesUrls = require('./routes/route');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const session = require('express-session');
-// var admin = require("firebase-admin");
-// var serviceAccount = require("C:\Users\Haleema Shahid\Desktop\CoLearn\firebase-admin-sdk\colearn-35de8-firebase-adminsdk-rblxy-3794af0748.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
+dotenv.config();
 
 app.use(session({
   secret: 'haleema1073',
@@ -19,23 +14,64 @@ app.use(session({
   saveUninitialized: false
 }));
 
-//const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(express.json());
+app.use(cors());
+app.use('/backend', routesUrls);
 
-//dotenv.config()
+const MONGODB_URI = "mongodb+srv://hatUser:Hat2023@cluster0.an4x4aw.mongodb.net/?retryWrites=true&w=majority";
 
-
-// const uri = "mongodb+srv://hatUser:Hat2023@cluster0.an4x4aw.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// //client.connect(()=>console.log("db connected"))
-// client.connect(err => {
-//   if (err) throw err;
-//   const collection = client.db("colearnDb").collection("user");
-//   client.close();
-// });
-
-app.use(express.json())
-app.use(cors())
-app.use('/backend', routesUrls)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(4000, () => console.log("Server is up and running"));
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 
-app.listen(4000, () => console.log("Server is up and running\n"))
+
+
+
+
+
+//....................................................................................................
+
+
+
+// const express = require('express')
+// const app = express()
+// //const mongoose = require('mongoose')
+// const routesUrls = require('./routes/route')
+// const dotenv = require('dotenv')
+// const cors = require('cors')
+// const session = require('express-session');
+
+
+
+// app.use(session({
+//   secret: 'haleema1073',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+// //const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// //dotenv.config()
+
+
+// // const uri = "mongodb+srv://hatUser:Hat2023@cluster0.an4x4aw.mongodb.net/?retryWrites=true&w=majority";
+// // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// // //client.connect(()=>console.log("db connected"))
+// // client.connect(err => {
+// //   if (err) throw err;
+// //   const collection = client.db("colearnDb").collection("user");
+// //   client.close();
+// // });
+
+// app.use(express.json())
+// app.use(cors())
+// app.use('/backend', routesUrls)
+
+
+// app.listen(4000, () => console.log("Server is up and running\n"))
