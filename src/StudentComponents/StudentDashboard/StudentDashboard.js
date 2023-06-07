a34a8a5aea7a8f61654488f5812bce36802952a2
aleen357 <59262976+aleen357@users.noreply.github.com>
2023-06-07 12:02:11 +0500

    student dash UI

--- src/StudentComponents/StudentDashboard/StudentDashboard.js
+++ src/StudentComponents/StudentDashboard/StudentDashboard.js
@@ -6,14 +6,32 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import DashboardHeader from '../DashboardHeader';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import createClassBg from '../../images/classaddcard.jpg'
+import sample from '../../images/sample.jpg'
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 function StudentDashboard() {
   const { userId } = useParams();
   const [showJoinClassModal, setShowJoinClassModal] = useState(false);
   const [classCode, setClassCode] = useState('');
   const [classes, setClasses] = useState([]);
+  const [joinNewClassButton, setJoinNewClassButton] = useState(false);
+  const [open, setOpen] = useState(false);
+
 
 
   const navigate = useNavigate();
@@ -37,6 +55,7 @@ function StudentDashboard() {
   }, [userId, classes]);
   const handleJoinClassClick = () => {
     setShowJoinClassModal(true);
+    setJoinNewClassButton(true)
   };
 
   const handleClassCodeChange = (event) => {
@@ -46,6 +65,11 @@ function StudentDashboard() {
 
   const handleJoinClassSubmit = async (event) => {
     event.preventDefault();
+
+    if (classes.some(cls => cls._id === classCode)) {
+      setOpen(true);
+      return;
+    }
 
     if (classCode === '') {
       // Class code is empty, do not attempt to join class
@@ -62,6 +86,7 @@ function StudentDashboard() {
         setClasses([...classes, data]);
         setShowJoinClassModal(false);
         setClassCode('');
+        setJoinNewClassButton(false);
       } else {
         // Class code does not exist
         console.error('Class code not found');
@@ -75,6 +100,7 @@ function StudentDashboard() {
 
 
   const handleCancelClick = () => {
+    setJoinNewClassButton(false);
     setShowJoinClassModal(false);
     setClassCode('')
   };
@@ -83,44 +109,172 @@ function StudentDashboard() {
     setClasses(classes.filter(cls => !(cls.name === name && cls.section === section)));
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+    setJoinNewClassButton(false)
+  };
+
   return (
-    <div className='TeacherDashboard'>
-      <DashboardHeader userId={userId} />
-      <button onClick={handleJoinClassClick} className="create-class-button">Join New Class</button>
+    <div className='TeacherDashboard' sx={{
+  backgroundColor: 'white',
+  padding: '0px 4px',
+  
+}}>
+
+  <DashboardHeader userId={userId} />
+
+  {!joinNewClassButton && (
+    
+   
+        <IconButton
+          onClick={handleJoinClassClick}
+          sx={{
+            backgroundColor: '#0E3386',
+            color: 'white',
+            position: 'fixed',
+            bottom: '30px',
+            right: '30px',
+            zIndex: 999,
+            boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+            transition: 'background-color 0.3s ease-in-out',
+            '@media (max-width: 600px)': {
+              bottom: '20px',
+              right: '20px',
+            },
+
+          }}
+        >
+          <AddIcon fontSize="large" sx={{ fontSize: '3rem' }} />
+        </IconButton>
+   
+    
+  )}
+
+      {/* <button onClick={handleCreateClassClick} className="create-class-button" hidden={createNewClassButton}>Create New Class</button> */}
+      {open && (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            You have already joined this class!
+          </Alert>
+        </Snackbar>
+      )}
+
       {showJoinClassModal && (
-        <div className="create-class-modal">
-          <form onSubmit={handleJoinClassSubmit} className="create-class-form">
-            <label htmlFor="class-name-input">Class Code:</label>
-            <input
-              id="class-name-input"
-              type="text"
-              value={classCode}
-              onChange={handleClassCodeChange}
-            />
-            <br />
-            <button type="submit" className='create-button'>Join</button>
-            <button onClick={handleCancelClick} className="cancel-button">
-              Cancel
-            </button>
-          </form>
+        <div style={{}}>
+          <Grid container justifyContent="center" style={{ width: '100%', height: '100%', position: 'fixed', top: 0, left: 0 }}>
+            <Grid item xs={12} md={6} lg={4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Paper elevation={3} style={{ padding: '40px', backgroundImage: `url(${createClassBg})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '90%' }}>
+                <Paper elevation={0} style={{ padding: '20px' }}>
+                  <form onSubmit={handleJoinClassSubmit}>
+                    <Stack spacing={2}>
+                      <TextField
+                        required
+                        id="classcode"
+                        label="Class Code"
+                        value={classCode}
+                        onChange={handleClassCodeChange}
+                        fullWidth
+                      />
+                     
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        style={{
+                          backgroundColor: '#0E3386',
+                          color: 'white',
+                          borderRadius: '10px',
+                          padding: '10px 30px',
+                          fontSize: '1rem',
+                          '&:hover': {
+                            backgroundColor: '#0E3386',
+                          },
+                        }}
+                      >
+                        Join Class
+                      </Button>
+                      <Button
+                        onClick={handleCancelClick}
+                        variant="outlined"
+                        sx={{ borderColor: '#0E3386', color: '#0E3386' }}
+                      >
+                        Cancel
+                      </Button>
+                    </Stack>
+                  </form>
+                </Paper>
+              </Paper>
+            </Grid>
+          </Grid>
         </div>
       )}
       {!showJoinClassModal && (
-        <div className="classes-grid">
-          {classes.map((classObj) => (
-            // <Link to={`/user/${userId}/class/${classObj.id}`} key={classObj.id}>
-            <div className="container_card" key={classObj.name + classObj.discription} >
-              <Link to={`/s/${userId}/class/${classObj._id}`} style={{ textDecoration: 'none' }}>
-                <StudentDashboardCard name={classObj.name} section={classObj.description} classId={classObj._id} userId={userId} onDelete={handleDeleteClass} />
-              </Link>
-            </div>
-            // </Link>
-          ))}
+        <div style={{
+          backgroundColor: 'white',
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))',
+          gridGap: '40px',
+          padding: '20px',
+
+        }}>
+          <Grid container spacing={3}>
+            {classes.map((classObj) => (
+              <Grid item xs={12} sm={6} md={4} key={classObj.name + classObj.description} style={{ height: '200px', width: '300px' }}>
+                <Link to={`/s/${userId}/class/${classObj._id}`} style={{ textDecoration: 'none' }}>
+                  <StudentDashboardCard name={classObj.name} section={classObj.description} classId={classObj._id} userId={userId} onDelete={handleDeleteClass} style={{ height: '500px' }} />
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
         </div>
-      )
-      }
-    </div >
+      )}
+
+    </div>
   );
 }
 
 export default StudentDashboard;
+
+
+  // return (
+  //   <div className='TeacherDashboard'>
+  //     <DashboardHeader userId={userId} />
+  //     <button onClick={handleJoinClassClick} className="create-class-button">Join New Class</button>
+  //     {showJoinClassModal && (
+  //       <div className="create-class-modal">
+  //         <form onSubmit={handleJoinClassSubmit} className="create-class-form">
+  //           <label htmlFor="class-name-input">Class Code:</label>
+  //           <input
+  //             id="class-name-input"
+  //             type="text"
+  //             value={classCode}
+  //             onChange={handleClassCodeChange}
+  //           />
+  //           <br />
+  //           <button type="submit" className='create-button'>Join</button>
+  //           <button onClick={handleCancelClick} className="cancel-button">
+  //             Cancel
+  //           </button>
+  //         </form>
+  //       </div>
+  //     )}
+  //     {!showJoinClassModal && (
+  //       <div className="classes-grid">
+  //         {classes.map((classObj) => (
+  //           // <Link to={`/user/${userId}/class/${classObj.id}`} key={classObj.id}>
+  //           <div className="container_card" key={classObj.name + classObj.discription} >
+  //             <Link to={`/s/${userId}/class/${classObj._id}`} style={{ textDecoration: 'none' }}>
+  //               <StudentDashboardCard name={classObj.name} section={classObj.description} classId={classObj._id} userId={userId} onDelete={handleDeleteClass} />
+  //             </Link>
+  //           </div>
+  //           // </Link>
+  //         ))}
+  //       </div>
+  //     )
+  //     }
+  //   </div >
+  // );
+
--- src/StudentComponents/StudentDashboard/StudentDashboardCard.js
+++ src/StudentComponents/StudentDashboard/StudentDashboardCard.js
@@ -1,36 +1,49 @@
 import * as React from 'react';
-import { stopPropagation } from 'react';
 import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Button } from '@mui/material';
 
-const styles = {
 
 
-}
+const StyledCard = styled(Card)(({ theme }) => ({
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  borderRadius: '20px',
 
-function StudentDashboardCard(props) {
 
+
+}));
+
+const StyledCardHeader = styled(CardHeader)(({ theme }) => ({
+  background: `linear-gradient(to right, #073980, #073980)`,
+  color: 'white',
+  padding: theme.spacing(3),
+  '& .MuiCardHeader-title': {
+    fontSize: '1.15rem',
+  },
+
+}));
+
+const StyledCardContent = styled(CardContent)(({ theme }) => ({
+  flex: '1 0 auto',
+}));
+
+function TeacherDashboardCard(props) {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleMenuOpen = (event) => {
-    event.stopPropagation(); // stop event propagation to the card
+    event.stopPropagation();
     event.preventDefault();
     setAnchorEl(event.currentTarget);
   };
@@ -39,28 +52,38 @@ function StudentDashboardCard(props) {
     e.stopPropagation();
     e.preventDefault();
     setAnchorEl(null);
+
+
   };
 
-  const handleDeleteClick = () => {
+
+
+
+
+  const [open, setOpen] = useState(false);
+
+  const handleDeleteClick = (e) => {
+    e.preventDefault();
+   // e.stopPropagation();
+    setOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
     props.onDelete(props.name, props.section);
+    setOpen(false);
     handleMenuClose();
   };
 
-  return (
-    <Card sx={{ height: 20 + "vh", maxwidth: 300 }}>
-      <CardHeader
-        sx={{
-          height: '50%',
-          background: `linear-gradient(to right, #1e3c72, #2a5298)`,
-          color: 'white',
-          justifyContent: 'center',
-          padding: '20px',
-          '& .MuiCardHeader-title': {
-            fontSize: '1.15rem', // Adjust the font size as needed
-          },
+  const handleCancelDelete = () => {
+    setOpen(false);
+  };
 
-        }}
+
+  return (
+    <StyledCard>
+      <StyledCardHeader
         action={
+
           <div>
             <div onClick={(e) => {
               e.preventDefault();
@@ -78,52 +101,178 @@ function StudentDashboardCard(props) {
                 open={Boolean(anchorEl)}
                 onClose={(e) => { handleMenuClose(e) }}
               >
-                {/* <MenuItem onClick={handleMenuClose}>View Assignments</MenuItem>
-                <MenuItem onClick={handleDeleteClick}>Leave Class</MenuItem> */}
+
+<Link to={`/s/${props.userId}/class/${props.classId}`}> <MenuItem >View Class</MenuItem></Link>
                 {/* <MenuItem onClick={handleMenuClose}>Copy Code</MenuItem> */}
-                {/* <Link to={`/user/${props.userID}/class/${props.id}`}> */}
-                <Link to={`/s/${props.userId}/class/${props.classId}`}> <MenuItem onClick={handleMenuClose}>View Class</MenuItem></Link>
-                {/* </Link> */}
                 <Link to={`/user/${props.userId}/class/${props.classId}/classAnalytics`}><MenuItem >Class Analytics</MenuItem></Link>
                 {/* <Link to={`/user/${props.userId}/class/${props.classId}/classLeaderboard`}><MenuItem>Leaderboard</MenuItem></Link> */}
+                <MenuItem onClick={(e) => { handleDeleteClick(e); }}>Leave</MenuItem>
+                <Dialog open={open} onClose={handleCancelDelete}>
+                  <DialogTitle>Confirmation</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>Are you sure you want to Leave?</DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleConfirmDelete} color="primary">Yes</Button>
+                    <Button onClick={handleCancelDelete} color="primary" autoFocus>No</Button>
+                  </DialogActions>
+                </Dialog>
               </Menu>
             </div>
           </div>
+
         }
         title={props.name}
-      //subheader={props.section}
       />
-      <CardContent>
+      <StyledCardContent>
         <Typography variant="body2" color="text.secondary">
           {props.section}
         </Typography>
-      </CardContent>
-    </Card>
+      </StyledCardContent>
 
-    // <div className="class-card">
-    //     <div className='class-name-container'>
-    //         <div className='dropdown'>
-    //             <div className='dropbtn optionContainer btn-right showLeft '>
-    //                 <li></li>
-    //                 <li></li>
-    //                 <li></li>
-    //             </div>
-    //             <div class="dropdown-content-forTeacher-class">
-    //                 <a href="#home">Delete</a>
-    //                 <a href="#about">Copy Code</a>
-    //                 <a href="#about">View CLO</a>
-    //                 <a href="#about">Class Analytics</a>
-    //                 <a href="#about">Leaderboard</a>
-    //             </div>
-    //         </div>
-
-    //         <h3 className="class-name">{props.name}</h3>
-    //     </div>
-    //     <div class-section-container>
-    //         <p className="class-section">{props.section}</p>
-    //     </div>
-    // </div>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        {/* <MenuItem onClick={(e) => { handleDeleteClick(e); }}>Leave</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Copy Code</MenuItem> */}
+        <Link to={`/s/${props.userId}/class/${props.classId}`}> <MenuItem >View Class</MenuItem></Link>
+        <Link to={`/user/${props.userId}/class/${props.classId}/classAnalytics`}><MenuItem >Class Analytics</MenuItem></Link>
+        <Link to={`/user/${props.userId}/class/${props.classId}/classLeaderboard`}><MenuItem>Leaderboard</MenuItem></Link>
+      </Menu>
+    </StyledCard>
   );
 }
 
-export default StudentDashboardCard
+export default TeacherDashboardCard;
+
+// import * as React from 'react';
+// import { stopPropagation } from 'react';
+// import { useState } from 'react';
+// import { styled } from '@mui/material/styles';
+// import Card from '@mui/material/Card';
+// import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
+// import CardContent from '@mui/material/CardContent';
+// import CardActions from '@mui/material/CardActions';
+// import Collapse from '@mui/material/Collapse';
+// import Avatar from '@mui/material/Avatar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import { red } from '@mui/material/colors';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ShareIcon from '@mui/icons-material/Share';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
+// import { Link } from 'react-router-dom';
+
+// const styles = {
+
+
+// }
+
+// function StudentDashboardCard(props) {
+
+//   const [anchorEl, setAnchorEl] = useState(null);
+
+//   const handleMenuOpen = (event) => {
+//     event.stopPropagation(); // stop event propagation to the card
+//     event.preventDefault();
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleMenuClose = (e) => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//     setAnchorEl(null);
+//   };
+
+//   const handleDeleteClick = () => {
+//     props.onDelete(props.name, props.section);
+//     handleMenuClose();
+//   };
+
+//   return (
+//     <Card sx={{ height: 20 + "vh", maxwidth: 300 }}>
+//       <CardHeader
+//         sx={{
+//           height: '50%',
+//           background: `linear-gradient(to right, #1e3c72, #2a5298)`,
+//           color: 'white',
+//           justifyContent: 'center',
+//           padding: '20px',
+//           '& .MuiCardHeader-title': {
+//             fontSize: '1.15rem', // Adjust the font size as needed
+//           },
+
+//         }}
+//         action={
+//           <div>
+//             <div onClick={(e) => {
+//               e.preventDefault();
+//             }}>
+//               <IconButton aria-label="settings" onClick={(e) => {
+//                 handleMenuOpen(e);
+//               }}>
+//                 <MoreVertIcon />
+//               </IconButton>
+
+
+
+//               <Menu
+//                 anchorEl={anchorEl}
+//                 open={Boolean(anchorEl)}
+//                 onClose={(e) => { handleMenuClose(e) }}
+//               >
+//                 {/* <MenuItem onClick={handleMenuClose}>View Assignments</MenuItem>
+//                 <MenuItem onClick={handleDeleteClick}>Leave Class</MenuItem> */}
+//                 {/* <MenuItem onClick={handleMenuClose}>Copy Code</MenuItem> */}
+//                 {/* <Link to={`/user/${props.userID}/class/${props.id}`}> */}
+//                 <Link to={`/s/${props.userId}/class/${props.classId}`}> <MenuItem >View Class</MenuItem></Link>
+//                 {/* </Link> */}
+//                 <Link to={`/user/${props.userId}/class/${props.classId}/classAnalytics`}><MenuItem >Class Analytics</MenuItem></Link>
+//                 {/* <Link to={`/user/${props.userId}/class/${props.classId}/classLeaderboard`}><MenuItem>Leaderboard</MenuItem></Link> */}
+//               </Menu>
+//             </div>
+//           </div>
+//         }
+//         title={props.name}
+//       //subheader={props.section}
+//       />
+//       <CardContent>
+//         <Typography variant="body2" color="text.secondary">
+//           {props.section}
+//         </Typography>
+//       </CardContent>
+//     </Card>
+
+//     // <div className="class-card">
+//     //     <div className='class-name-container'>
+//     //         <div className='dropdown'>
+//     //             <div className='dropbtn optionContainer btn-right showLeft '>
+//     //                 <li></li>
+//     //                 <li></li>
+//     //                 <li></li>
+//     //             </div>
+//     //             <div class="dropdown-content-forTeacher-class">
+//     //                 <a href="#home">Delete</a>
+//     //                 <a href="#about">Copy Code</a>
+//     //                 <a href="#about">View CLO</a>
+//     //                 <a href="#about">Class Analytics</a>
+//     //                 <a href="#about">Leaderboard</a>
+//     //             </div>
+//     //         </div>
+
+//     //         <h3 className="class-name">{props.name}</h3>
+//     //     </div>
+//     //     <div class-section-container>
+//     //         <p className="class-section">{props.section}</p>
+//     //     </div>
+//     // </div>
+//   );
+// }
+
+// export default StudentDashboardCard
